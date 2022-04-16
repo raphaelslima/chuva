bac8e3ae1b1daa0d857e57c51abf3d48d5d8cb09
Raphael Lima <raphaeldesousalm@gmail.com>
2022-04-16 10:43:27 -0300

    Contrução do footer de html

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,12 +4,18 @@ import React from "react";
 import './style.css'
 
 //image
-import imgFooter from '../../assets/img/footer.png'
+import footerLogo from '../../assets/img/footerImg.PNG'
 
 export default function Footer(props){
   return(
       <footer className="footer">
-         <img src={imgFooter}/>
+         <img src={footerLogo}/>
+         <div className="text">
+           <p> <strong> Preservar a memória do evento e ampliar o acesso ao conhecimento científico </strong> gerado em eventos é a rezão de ser da plataforma Galoá Proceedings</p>
+
+           <p> Os trabalhos públicos aqui têm um maior alcance e ficam disponiveis para toda a comunidade científicamantendo acesso a debate científico fomentado pelos encontros e aumentando o impacto do evento.</p>
+         </div>
+         
       </footer>
   
   )
